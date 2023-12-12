6b26567b1fa506fe292f9fdc03af011ec3d93055
ParvDev <parvdev0109@gmail.com>
2023-12-12 05:41:15 +0530

    First Comit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+import "./App.css"
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
